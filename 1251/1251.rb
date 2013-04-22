2283d1992220477843dbff0c328848c97bb4dd5f
Hiroyuki Sano <sh19910711@gmail.com>
2013-04-22 19:42:26 +0900

    AOJ 1251: Unsubmitted

--- 1251/1251.rb
+++ 1251/1251.rb
@@ -1,121 +1,119 @@
 require 'set'
 
-class String
-    def to_path
-        self.concat 'index.html' if self != '/' && self[-1, 1] == '/'
-        self.strip.split('/')[1..-1]
-    end
+def get_path
+    gets.strip
 end
 
-class Path
-    def initialize path
-        @path = path
-    end
-    def to_s
-        '/' + @path.join('/')
-    end
-    def to_strict_path
-        sub = []
-        @path.each {|item|
-            if item == '..'
-                sub.pop
-            elsif item != '.'
-                sub.push item
-            end
-        }
-        sub
-    end
+def get_s_path path
+    "/" + path.join('/')
 end
 
 class Solution
 
     def initialize
-        @n = 0
-        @m = 0
-        @files = Set.new []
-        @dirs = Set.new []
+        @n, @m = 0, 0
+        @dirs = Set.new
+        @files = Set.new
     end
 
     def input
         @n, @m = gets.split(' ').map {|v| v.to_i}
-        @dirs.add(Path.new([]).to_s)
-        @n.times {|| add_file gets.strip.to_path }
+        @n.times {|| add_file get_path }
         @n != 0 || @m != 0
     end
 
+    def add_index_html s_path
+        s_path += "index.html" if s_path[-1, 1] == "/"
+        path = s_path.split('/')[1..-1]
+        s_path += "/index.html" if path[-1, 1][0] == ".." || path[-1, 1][0] == "."
+        s_path
+    end
+
+    def convert s_path
+        s_path.split('/')[1..-1]
+    end
+
+    def is_invalid_subdir path
+        cur = []
+        path.pop
+        path.any? {|item|
+            if item == ".."
+                cur.pop
+            elsif item != "."
+                cur.push item
+            end
+            s_cur = get_s_path cur
+            ! @dirs.include? s_cur 
+        }
+    end
+
+    def is_invalid_file path
+        s_path = get_s_path path
+        puts "s_path = #{s_path}, #{@files.include? s_path}"
+        ! @files.include? s_path
+    end
+
+    def is_over path
+        depth = 0
+        path.any? {|item|
+            if item == ".."
+                depth -= 1
+            elsif item != "."
+                depth += 1
+            end
+            depth < 0
+        }
+    end
+
+    def query a, b
+        a = add_index_html a
+        b = add_index_html b
+        puts "a = #{a}, b = #{b}"
+        path1, path2 = convert(a), convert(b)
+        return :NOT_FOUND if is_over(Array.new(path1)) || is_over(Array.new(path2))
+        return :NOT_FOUND if is_invalid_subdir(Array.new(path1)) || is_invalid_subdir(Array.new(path2))
+        return :NOT_FOUND if is_invalid_file(Array.new(path1)) || is_invalid_file(Array.new(path2))
+        puts "path1 = #{path1}, path2 = #{path2}"
+        return :YES
+    end
+
     def output result
-        case result
-        when :NOT_FOUND
-            puts 'not found'
-        when :YES
-            puts 'yes'
-        when :NO
-            puts 'no'
+        if result == :NOT_FOUND
+            puts "not found"
+        elsif result == :YES
+            puts "yes"
+        else
+            puts "no"
         end
+        puts ""
+    end
+
+    def add_file s_path
+        path = convert s_path
+        s_filepath = get_s_path path
+        @files.add s_filepath
+        path.pop
+
+        cur = []
+        path.each {|item|
+            if item == ".."
+                cur.pop
+            elsif item != "."
+                cur.push item
+            end
+            s_path = get_s_path cur
+            @dirs.add s_path
+        }
     end
 
     def fetch_query
-        @m.times {|| output solve gets.strip.to_path, gets.strip.to_path }
+        @m.times {|| output query get_path, get_path }
     end
 
-    def solve path1, path2
-        path1 = [] if path1 == nil
-        path2 = [] if path2 == nil
-        return :NOT_FOUND if ( ! check_valid_subdir(path1[0..-2]) || ! check_valid_subdir(path2[0..-2]) )
-        return :NOT_FOUND if ( ! exist_file(path1) || ! exist_file(path2) )
-        s_path1 = get_strict_path path1
-        s_path2 = get_strict_path path2
-        s_path1 == s_path2 ? :YES : :NO
-    end
-
-    def get_strict_path path
-        res = Path.new(path).to_strict_path
-        return res if @files.include? Path.new(res).to_s
-        res.push 'index.html'
-        res
-    end
-
-    def exist_file path
-        strict_path = Path.new(path).to_strict_path
-        return true if @files.include? Path.new(strict_path).to_s
-        strict_path.push 'index.html'
-        @files.include? Path.new(strict_path).to_s
-    end
-
-    def check_valid_subdir path
-        sub = []
-        return true if path == nil || path.empty?
-        path.all? {|item|
-            if item == '..'
-                sub.pop
-            elsif item != '.'
-                sub.push item
-            end
-            @dirs.include?(Path.new(sub).to_s)
-        }
-    end
-
-    def add_subdir path
-        sub = []
-        path.each {|item|
-            if item == '..'
-                sub.pop
-            elsif item != '.'
-                sub.push item
-            end
-            @dirs.add Path.new(sub).to_s
-        }
-    end
-
-    def add_file path
-        @files.add Path.new(path).to_s
-        add_subdir path[0..-2]
-    end
 end
 
 s = Solution.new
 while s.input do
     s.fetch_query
 end
-
 
